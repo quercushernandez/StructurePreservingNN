fe3269ca96cfae7426d037211a5b5f16b1a2dea6
Quercus <63294776+quercushernandez@users.noreply.github.com>
2021-03-11 17:45:12 +0100

    Update and rename run_viscolastic_train.sh to run_viscoelastic_train.sh

---
+++ executables/run_viscoelastic_train.sh
@@ -0,0 +1,5 @@
+
+python main.py --sys_name viscoelastic --train True --net_init kaiming_uniform \
+    --hidden_vec 50 50 50 50 50 \
+    --miles 500 1000 --gamma 0.1 --max_epoch 1500 \
+    --dset_norm False
