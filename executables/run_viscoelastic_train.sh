88405249f5adbf057c2dec9eec0aae144fbc6a42
Quercus <63294776+quercushernandez@users.noreply.github.com>
2022-05-30 16:20:38 +0200

    Update run_viscoelastic_train.sh

--- executables/run_viscoelastic_train.sh
+++ executables/run_viscoelastic_train.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 python main.py --sys_name viscoelastic --train True --net_init kaiming_uniform \
     --hidden_vec 50 50 50 50 50 \
     --miles 500 1000 --gamma 0.1 --max_epoch 1500 \
