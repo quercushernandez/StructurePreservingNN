eebb17ab327fbd80965da1b1c68dc9bd3a84caf1
Quercus <63294776+quercushernandez@users.noreply.github.com>
2022-05-30 16:20:08 +0200

    Update run_double_pendulum_train.sh

--- executables/run_double_pendulum_train.sh
+++ executables/run_double_pendulum_train.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 python main.py --sys_name double_pendulum --train True --net_init kaiming_uniform \
     --hidden_vec 200 200 200 200 200 \
     --miles 600 1200 --gamma 0.1 --max_epoch 1800
